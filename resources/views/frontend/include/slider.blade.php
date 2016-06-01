1dbaa23687ae1ffa61042303e4a934ffa7bbbf3c
Ruwan Ranganath <ruwanranganathz@gmail.com>
2016-06-01 14:47:07 +0530

    slider to dynamic page

--- resources/views/frontend/include/slider.blade.php
+++ resources/views/frontend/include/slider.blade.php
@@ -1,105 +1,40 @@
-
- <!-- *** NAVBAR ***
- _________________________________________________________ -->
-
-    <div class="navbar navbar-default yamm" role="navigation" id="navbar">
-        <div class="container">
-            <div class="navbar-header">
-
-                <a class="navbar-brand home" href="/" data-animate-hover="bounce">
-                    <img src="{{ url('frontres/img/logo.png') }}" alt="Obaju logo" class="hidden-xs" style="border-radius: 50%;width: 121px;margin-top: -37px;">
-                    <img src="{{ url('frontres/img/logo-small.png') }}" alt="Obaju logo" class="visible-xs img-circle img-responsive" style="width: 29%;">
-                </a>
-                <div class="navbar-buttons">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
-                        <span class="sr-only">Toggle navigation</span>
-                        <i class="fa fa-align-justify"></i>
-                    </button>
-                    <button type="button" class="navbar-toggle"  data-toggle="modal" data-target="#search-modal">
-                        <span class="sr-only">Toggle search</span>
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-            <!--/.navbar-header -->
-
-            <div class="navbar-collapse collapse DD" id="navigation" style="">
-
-                <ul class="nav navbar-nav navbar-left">
-
-                <li class="active"><a href="{{url('/')}}">Home</a></li>
-                <li class="#"><a href="{{url('newsfeed')}}">News Feed</a></li>
-                <li class="#"><a href="{{url('profile')}}">Profile</a></li>
-                <li class="#"><a href="{{url('about')}}">About Us</a></li>
-                 <li class="#"><a href="{{url('contact')}}">Contact Us</a></li>
-                </ul>
-
-            </div>
-            <!--/.nav-collapse -->
-
-            <div class="navbar-buttons">
-                <!--/.nav-collapse -->
-
-                <div class="navbar-collapse collapse right">
-                    <button type="button" class="btn navbar-btn btn-primary"   data-toggle="modal" data-target="#search-modal">
-                        <span class="sr-only">Toggle search</span>
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-
-            </div>
-            
-            <div class="modal fade" id="search-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
-            <div class="modal-dialog modal-md">
-
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title text-center"><i class="fa fa-search" aria-hidden="true"></i> SEARCH</h4>
+@if (Auth::guest())
+<div class="container">
+    <div class="col-md-3">
+        <div class="box same-height" style="background: #2072bc;">
+            {!! Form::open(['url' => 'register']) !!}
+                <div class="row" style="margin-top: -7%;">
+                    <div class="col-md-12">
+                        <h4 align="center"> Create Your Own Profile </h4>
+                        <h5 align="center" style="color:#fff;font-size: 12px;">Lorem Ipsum is simply dummy text of t</h5>
                     </div>
-                    <div class="modal-body">
-                        <form action="" method="post">
-                            
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label class="col-md-4 col-xs-4" for="name">Name</label>
-                                        <div class="col-md-4 col-xs-4">
-                                            <input type="radio"/> Bride
-                                        </div>
-                                        <div class="col-md-4 col-xs-4">
-                                            <input type="radio"/> Groom
-                                        </div>
-                                    </div> 
-                                </div>      
-                            </div>
-                            
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label class="col-md-4 col-xs-4" for="name">Age</label>
-                                        <div class="col-md-4 col-xs-4">
-                                            <select class="form-control LL" id="age">
-                                                <option>Select</option>
-                                            </select>
-                                        </div>
-                                        <div class="col-md-4 col-xs-4">
-                                            <select class="form-control LL" id="age">
-                                                <option>Select</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            
-                            
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label class="col-md-4 col-xs-4" for="name">Country</label>
-                                        <div class="col-md-8 col-xs-8">
-                                             <select name="country">
+                    <div class="col-sm-12">
+                        <div class="form-group">
+
+                            <input name="name" type="text" class="form-control" placeholder="User Name" id="User Name">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+
+                            <input name="password" type="password" class="form-control" placeholder="Password" id="Password">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+
+                            <input name="password_confirmation" type="password" class="form-control" placeholder="Conform Password" id="password">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+
+                            <input name="email" type="email" class="form-control" placeholder="Email" id="email">
+                        </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <select name="country">
 <option value="">Country...</option>
 <option value="Afganistan">Afghanistan</option>
 <option value="Albania">Albania</option>
@@ -349,35 +284,58 @@
 <option value="Zambia">Zambia</option>
 <option value="Zimbabwe">Zimbabwe</option>
 </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="form-group">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="col-md-10">
-                                            Only Profiels With Photo <input type="radio"/>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button type="search" class="btn btn-primary HH" id="search">Search</button>
-                                        </div>
-                                        
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                          </div>
+                    </div>
+                    <div class="col-sm-12">
+                        <div class="form-group">
+                            <input type="checkbox" name="terms" value="terms" id="terms">
+                            <span style="color:#fff; font-size: 11px;">I have read and accept the terms of use.</span>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 text-center">
+                        <button type="submit" class="btn btn-primary" id="sign-up"></button>
                     </div>
                 </div>
+                <!-- /.row -->
+          {!! Form::close() !!}
+        </div>
+    </div>
+    <div class="col-md-9">
+        <div id="main-slider">
+            <div class="item">
+                <img src="{{ url('frontres/img/main-slider1.jpg') }}" alt="" class="img-responsive">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider2.jpg') }}" alt="">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider3.jpg') }}" alt="">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider4.jpg') }}" alt="">
             </div>
         </div>
-            
-            <!--/.nav-collapse -->
-
-        </div>
-        <!-- /.container -->
+        <!-- /#main-slider -->
     </div>
-    <!-- /#navbar -->
-
-    <!-- *** NAVBAR END *** -->
+</div>
+@else
+<div class="container">
+    <div class="col-md-12">
+        <div id="main-slider">
+            <div class="item">
+                <img src="{{ url('frontres/img/main-slider1.jpg') }}" alt="" class="img-responsive">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider2.jpg') }}" alt="">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider3.jpg') }}" alt="">
+            </div>
+            <div class="item">
+                <img class="img-responsive" src="{{ url('frontres/img/main-slider4.jpg') }}" alt="">
+            </div>
+        </div>
+        <!-- /#main-slider -->
+    </div>
+</div>
+@endif
