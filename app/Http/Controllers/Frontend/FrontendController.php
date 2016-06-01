1c6da0a63f2a0e716d1c4042895dfe4fd61d3361
Ruwan Ranganath <ruwanranganathz@gmail.com>
2016-06-01 23:43:48 +0530

    changed

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Access\User\User;
+use Auth;
+use DB;
 
 /**
  * Class FrontendController
@@ -17,33 +19,135 @@ class FrontendController extends Controller
     public function index()
     {
 
+      // if the user logged in 
+      if (Auth::check())
+    {
+          $users_msg = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->count();
+
+          $users_msgs = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->latest('created_at')
+        ->distinct()->get();
+
+         // if the user logged in 
+
+        $users = User::orderByRaw("RAND()")->limit('6')->get();
+        return view('frontend.index',compact('users','users_msg','users_msgs'));
+
+    } else {
+     // if the user logged in 
+
         $users = User::orderByRaw("RAND()")->limit('6')->get();
         return view('frontend.index',compact('users'));
+
+  }
+
+
+
     }
 
-    /**
-     * @return \Illuminate\View\View
-     */
-    public function macros()
-    {
-        return view('frontend.macros');
-    }
 
     public function newsfeed()
     {
+
+
+
+       if (Auth::check())
+    {
+          $users_msg = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->count();
+
+
+                  $users_msgs = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->latest('created_at')
+        ->distinct()->get();
+
+
+         // if the user logged in 
+
+    
+
+      $users = User::latest('created_at')->limit('8')->get(); // get all product
+
+        return view('frontend.newsfeed',compact('users','users_msg','users_msgs'));
+
+
+    } else {
+
       $users = User::latest('created_at')->limit('8')->get(); // get all product
 
         return view('frontend.newsfeed',compact('users'));
+
+
+    }
+
+
+
     }
 
     public function about()
     {
-        return view('frontend.about');
+
+
+           if (Auth::check())
+    {
+          $users_msg = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->count();
+
+
+                  $users_msgs = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->latest('created_at')
+        ->distinct()->get();
+
+
+         // if the user logged in 
+
+    
+        return view('frontend.about',compact('users_msg','users_msgs'));
+   
+    } else {
+
+       return view('frontend.about');
+    }
+
+//wha
+
+
+       
     }
 
     public function contact()
     {
-              return view('frontend.contact');
+
+       if (Auth::check())
+    {
+          $users_msg = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->count();
+
+
+                  $users_msgs = DB::table('messages')
+        ->where('received_usr_id', '=', \Auth::user()->id)
+        ->latest('created_at')
+        ->distinct()->get();
+
+
+         // if the user logged in 
+
+    
+        return view('frontend.contact',compact('users_msg','users_msgs'));
+   
+    } else {
+
+      return view('frontend.contact');
+    }
+              
     }
 
 
